72a98b9723d7442b3dcfb6614f64f6a941448668
David Sn <divad.nnamtdeis@gmail.com>
2018-02-25 14:49:47 +0100

    Move channel id to secrets and adjust for Drone CI

--- upload.sh
+++ upload.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cp app/build/outputs/apk/debug/app-debug.apk Lawnchair-$MAJOR_MINOR.$TRAVIS_BUILD_NUMBER.apk
-cp app/build/outputs/mapping/debug/mapping.txt proguard-$MAJOR_MINOR.$TRAVIS_BUILD_NUMBER.txt
+cp app/build/outputs/apk/debug/app-debug.apk Lawnchair-alpha_$DRONE_BUILD_NUMBER.apk
+cp app/build/outputs/mapping/debug/mapping.txt proguard-alpha_$DRONE_BUILD_NUMBER.txt
 
-curl -F chat_id="-1001083653933" -F document=@"Lawnchair-$MAJOR_MINOR.$TRAVIS_BUILD_NUMBER.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
-curl -F chat_id="-1001083653933" -F text="changelog.txt" -F parse_mode="HTML" https://api.telegram.org/bot$BOT_TOKEN/sendMessage
-curl -F chat_id="442800997" -F document=@"proguard-$MAJOR_MINOR.$TRAVIS_BUILD_NUMBER.txt" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
+curl -F chat_id="$CHANNEL_ID" -F document=@"Lawnchair-alpha_$DRONE_BUILD_NUMBER.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
+curl -F chat_id="$CHANNEL_ID" -F text="changelog.txt" -F parse_mode="HTML" https://api.telegram.org/bot$BOT_TOKEN/sendMessage
+curl -F chat_id="442800997" -F document=@"proguard-alpha_$DRONE_BUILD_NUMBER.txt" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
