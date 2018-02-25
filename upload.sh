c000cb34180c65db9d9e9caaa2f3a256686ced06
David Sn <divad.nnamtdeis@gmail.com>
2018-02-25 21:01:21 +0100

    Escape delete

--- upload.sh
+++ upload.sh
@@ -5,7 +5,7 @@ if [ -z "$MAJOR_MINOR" ]; then
 fi
 
 # Adding body to changelog
-CHANGELOG="\<b\>Changelog for build ${MAJOR_MINOR}-${DRONE_BUILD_NUMBER}\</b\>
+CHANGELOG="\<b>Changelog for build ${MAJOR_MINOR}-${DRONE_BUILD_NUMBER}</b>
 $(cat changelog.txt)"
 
 # Preparing files to upload
