230f41cab19b78ed88dd31ef0c656f2a95f4e3ba
David Sn <divad.nnamtdeis@gmail.com>
2018-02-25 16:37:22 +0100

    Send seperator sticker before posting build

--- upload.sh
+++ upload.sh
@@ -8,6 +8,7 @@ fi
 cp app/build/outputs/apk/debug/app-debug.apk Lawnchair-alpha_$DRONE_BUILD_NUMBER.apk
 cp app/build/outputs/mapping/debug/mapping.txt proguard-alpha_$DRONE_BUILD_NUMBER.txt
 
+curl -F chat_id="$CHANNEL_ID" -F sticker="CAADBQADKAADTBCSGmapM3AUlzaHAg" https://api.telegram.org/bot$BOT_TOKEN/sendSticker
 curl -F chat_id="$CHANNEL_ID" -F document=@"Lawnchair-alpha_$DRONE_BUILD_NUMBER.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
 curl -F chat_id="$CHANNEL_ID" -F text=@"changelog.txt" -F parse_mode="HTML" https://api.telegram.org/bot$BOT_TOKEN/sendMessage
 curl -F chat_id="442800997" -F document=@"proguard-alpha_$DRONE_BUILD_NUMBER.txt" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
