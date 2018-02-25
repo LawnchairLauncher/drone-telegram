fed65e577f305b460a1b87138e24cf15fa1e8f20
David Sn <divad.nnamtdeis@gmail.com>
2018-02-25 16:59:38 +0100

    Please changelog, work nau!

--- upload.sh
+++ upload.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-# Check if changelog.txt exists, otherwise create a empty one
-if [ ! -f "changelog.txt" ]; then
-    echo "<b>Changelog for build ${MAJOR_MINOR}-${DRONE_BUILD_NUMBER}</b>${NEWLINE}No changelog found!" > changelog.txt
+if [ -z "$MAJOR_MINOR" ]; then
+  MAJOR_MINOR="alpha"
 fi
 
-cp app/build/outputs/apk/debug/app-debug.apk Lawnchair-alpha_$DRONE_BUILD_NUMBER.apk
-cp app/build/outputs/mapping/debug/mapping.txt proguard-alpha_$DRONE_BUILD_NUMBER.txt
+# Adding body to changelog
+CHANGELOG="<b>Changelog for build ${MAJOR_MINOR}-${DRONE_BUILD_NUMBER}</b>${NEWLINE}$(cat changelog.txt)"
 
+# Preparing files to upload
+cp app/build/outputs/apk/debug/app-debug.apk Lawnchair-${MAJOR_MINOR}_$DRONE_BUILD_NUMBER.apk
+cp app/build/outputs/mapping/debug/mapping.txt proguard-${MAJOR_MINOR}_$DRONE_BUILD_NUMBER.txt
+
+# Post build on Telegram
 curl -F chat_id="$CHANNEL_ID" -F sticker="CAADBQADKAADTBCSGmapM3AUlzaHAg" https://api.telegram.org/bot$BOT_TOKEN/sendSticker
-curl -F chat_id="$CHANNEL_ID" -F document=@"Lawnchair-alpha_$DRONE_BUILD_NUMBER.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
-curl -F chat_id="$CHANNEL_ID" -F text="$(cat changelog.txt)" -F parse_mode="HTML" https://api.telegram.org/bot$BOT_TOKEN/sendMessage
-curl -F chat_id="442800997" -F document=@"proguard-alpha_$DRONE_BUILD_NUMBER.txt" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
+curl -F chat_id="$CHANNEL_ID" -F document=@"Lawnchair-${MAJOR_MINOR}_$DRONE_BUILD_NUMBER.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
+curl -F chat_id="$CHANNEL_ID" -F text="$CHANGELOG" -F parse_mode="HTML" https://api.telegram.org/bot$BOT_TOKEN/sendMessage
+curl -F chat_id="442800997" -F document=@"proguard-${MAJOR_MINOR}_$DRONE_BUILD_NUMBER.txt" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
